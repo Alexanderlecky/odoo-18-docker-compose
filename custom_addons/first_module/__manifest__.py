7d06ab9d161da827211be3f0bc73296208d6af74
Alexanderlecky <alexanderkaranja78@gmail.com>
2025-04-02 09:43:46 +0300

    committed

---
+++ custom_addons/first_module/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'First Module',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'First Module Summary',
+    'sequence': 10,
+    'license': 'LGPL-3',
+    'author': 'Alexander',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/first_module.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
